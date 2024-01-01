4ae9d8e238606c8e11bdc9ec58ad39bf49b60ce7
Newton Nyamu <newtonnyamu@Newtons-MacBook-Pro.local>
2024-01-01 13:44:06 +0300

    add music template to the app

--- src/App.vue
+++ src/App.vue
@@ -37,10 +37,10 @@
       </div>
     </div>
 
-    <!-- <img
+    <img
       class="relative block mx-auto mt-5 -mb-20 w-auto max-w-full"
       src="/assets/img/introduction-music.png"
-    /> -->
+    />
   </section>
 
   <!-- Main Content -->
@@ -425,5 +425,3 @@
     </div>
   </div>
 </template>
-
-<script></script>
--- src/main.js
+++ src/main.js
@@ -1,10 +1,11 @@
-import './assets/base.css'
-
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
+
+import './assets/base.css'
+import './assets/main.css'
 
 const app = createApp(App)
 
