39078834ae980e503a016501ed6ee7d1ea460ca6
Newton Nyamu <newtonnyamu@Newtons-MacBook-Pro.local>
2024-01-01 15:36:52 +0300

    add function to close the pop up register modal

--- src/components/AppAuth.vue
+++ src/components/AppAuth.vue
@@ -22,8 +22,8 @@
           <!--Title-->
           <div class="flex justify-between items-center pb-4">
             <p class="text-2xl font-bold">Your Account</p>
-            <!-- Modal Close Button -->
-            <div class="modal-close cursor-pointer z-50">
+            <!-- Modal Close Button -->   
+            <div class="modal-close cursor-pointer z-50" @click.prevent="isOpen =false">
               <i class="fas fa-times"></i>
             </div>
           </div>
@@ -145,14 +145,15 @@
 </template>
 
 <script>
-import { mapState } from 'pinia'
+import { mapState, mapWritableState } from 'pinia'
 import useModalStore from '@/stores/modal'
 
 export default {
   name: 'AppAuth',
 
   computed: {
-    ...mapState(useModalStore, ['hiddenClass'])
+    ...mapState(useModalStore, ['hiddenClass']),
+    ...mapWritableState(useModalStore, ['isOpen'])
   }
 }
 </script>
