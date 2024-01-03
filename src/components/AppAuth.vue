4b17f15ceab2ae53046214faa2861645b39d48c5
Newton Nyamu <newtonnyamu@Newtons-MacBook-Pro.local>
2024-01-03 08:47:13 +0300

    add custom error for form validation

--- src/components/AppAuth.vue
+++ src/components/AppAuth.vue
@@ -1,9 +1,6 @@
 <template>
   <!-- Auth Modal -->
-  <div class="fixed z-10 inset-0 overflow-y-auto" 
-      id="modal" 
-      :class="hiddenClass"
-  >
+  <div class="fixed z-10 inset-0 overflow-y-auto" id="modal" :class="hiddenClass">
     <div
       class="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0"
     >
@@ -22,8 +19,8 @@
           <!--Title-->
           <div class="flex justify-between items-center pb-4">
             <p class="text-2xl font-bold">Your Account</p>
-            <!-- Modal Close Button -->   
-            <div class="modal-close cursor-pointer z-50" @click.prevent="isOpen =false">
+            <!-- Modal Close Button -->
+            <div class="modal-close cursor-pointer z-50" @click.prevent="isOpen = false">
               <i class="fas fa-times"></i>
             </div>
           </div>
@@ -36,21 +33,23 @@
                 href="#"
                 @click.prevent="tab = 'login'"
                 :class="{
-                     'hover:text-white text-white bg-blue-600' :tab == 'login',
-                     'hover:text-blue-600' :tab == 'register'
+                  'hover:text-white text-white bg-blue-600': tab == 'login',
+                  'hover:text-blue-600': tab == 'register'
                 }"
                 >Login
-                </a
-              >
+              </a>
             </li>
             <li class="flex-auto text-center">
-              <a class="block rounded py-3 px-4 transition" 
-                 href="#"
-                 @click.prevent="tab = 'register'"
-                 :class="{
-                     'hover:text-white text-white bg-blue-600' :tab == 'register',
-                     'hover:text-blue-600' :tab == 'login'}"
-                 >Register</a>
+              <a
+                class="block rounded py-3 px-4 transition"
+                href="#"
+                @click.prevent="tab = 'register'"
+                :class="{
+                  'hover:text-white text-white bg-blue-600': tab == 'register',
+                  'hover:text-blue-600': tab == 'login'
+                }"
+                >Register</a
+              >
             </li>
           </ul>
 
@@ -82,69 +81,93 @@
             </button>
           </vee-form>
           <!-- Registration Form -->
-          <vee-form v-else :validation-schema="schema">
+          <vee-form v-else :validation-schema="schema" @submit="register">
             <!-- Name -->
             <div class="mb-3">
               <label class="inline-block mb-2">Name</label>
               <vee-field
-                type="text" name ="name"
+                type="text"
+                name="name"
                 class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition duration-500 focus:outline-none focus:border-black rounded"
                 placeholder="Enter Name"
-                
               />
               <!--Error component from vee-validate-->
-              <ErrorMessage class="text-red-600" name = "name"/>
+              <ErrorMessage class="text-red-600" name="name" />
             </div>
             <!-- Email -->
             <div class="mb-3">
               <label class="inline-block mb-2">Email</label>
-              <input
+              <vee-field
                 type="email"
+                name="email"
                 class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition duration-500 focus:outline-none focus:border-black rounded"
                 placeholder="Enter Email"
               />
+              <!--Error component from vee-validate-->
+              <ErrorMessage class="text-red-600" name="email" />
             </div>
             <!-- Age -->
             <div class="mb-3">
               <label class="inline-block mb-2">Age</label>
-              <input
+              <vee-field
                 type="number"
+                name="age"
                 class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition duration-500 focus:outline-none focus:border-black rounded"
               />
+              <!--Error component from vee-validate-->
+              <ErrorMessage class="text-red-600" name="age" />
             </div>
             <!-- Password -->
             <div class="mb-3">
               <label class="inline-block mb-2">Password</label>
-              <input
+              <vee-field
                 type="password"
+                name="password"
                 class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition duration-500 focus:outline-none focus:border-black rounded"
                 placeholder="Password"
               />
+              <!--Error component from vee-validate-->
+              <ErrorMessage class="text-red-600" name="password" />
             </div>
             <!-- Confirm Password -->
             <div class="mb-3">
               <label class="inline-block mb-2">Confirm Password</label>
-              <input
+              <vee-field
                 type="password"
+                name="confirm_password"
                 class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition duration-500 focus:outline-none focus:border-black rounded"
                 placeholder="Confirm Password"
               />
+              <!--Error component from vee-validate-->
+              <ErrorMessage class="text-red-600" name="confirm_password" />
             </div>
             <!-- Country -->
             <div class="mb-3">
               <label class="inline-block mb-2">Country</label>
-              <select
+              <vee-field
+                as="select"
+                name="country"
                 class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition duration-500 focus:outline-none focus:border-black rounded"
               >
                 <option value="USA">USA</option>
                 <option value="Mexico">Mexico</option>
                 <option value="Germany">Germany</option>
-              </select>
+                <option value="Nairobi">Nairobi</option>
+              </vee-field>
+              <!--Error component from vee-validate-->
+              <ErrorMessage class="text-red-600" name="country" />
             </div>
             <!-- TOS -->
             <div class="mb-3 pl-6">
-              <input type="checkbox" class="w-4 h-4 float-left -ml-6 mt-1 rounded" />
+              <vee-field
+                type="checkbox"
+                name="tos"
+                value="1"
+                class="w-4 h-4 float-left -ml-6 mt-1 rounded"
+              />
               <label class="inline-block">Accept terms of service</label>
+              <!--Error component from vee-validate-->
+              <ErrorMessage class="text-red-600 block" name="tos" />
             </div>
             <button
               type="submit"
@@ -167,22 +190,28 @@ export default {
   name: 'AppAuth',
   data() {
     return {
-        tab: 'login',
-        schema: {
-            name: "required",
-            email: "",
-            password: "",
-            confirm_password: "",
-            tos: "",
-            age: "",
-            country: ""
-        }
+      tab: 'login',
+      schema: {
+        name: 'required|min:3|max:50|alpha_spaces',
+        email: 'required|email|min:3|max:50',
+        password: 'required|min:3|max:50',
+        confirm_password: 'password_mismatch:@password',
+        tos: 'tos',
+        age: 'required|min_value:18|max_value:100',
+        country: 'required|country_excluded:Nairobi'
+      }
     }
   },
 
   computed: {
     ...mapState(useModalStore, ['hiddenClass']),
     ...mapWritableState(useModalStore, ['isOpen'])
+  },
+
+  methods: {
+    register(value) {
+      console.log(value)
+    }
   }
 }
 </script>
--- src/includes/validation.js
+++ src/includes/validation.js
@@ -1,5 +1,16 @@
-import { Form as VeeForm, Field as VeeField, defineRule, ErrorMessage} from "vee-validate"
-import {required} from  "@vee-validate/rules"
+import { Form as VeeForm, Field as VeeField, defineRule, ErrorMessage, configure} from "vee-validate"
+import {
+  required,
+  alpha_spaces as alphaSpaces,
+  min,
+  max,
+  email,
+  integer,
+  max_value as maxValue,
+  min_value as minValue,
+  confirmed,
+  not_one_of as excluded
+} from '@vee-validate/rules'
 
 
 export default {
@@ -9,5 +20,38 @@ export default {
         app.component('ErrorMessage', ErrorMessage)
 
         defineRule('required', required)
+        defineRule('tos', required)
+        defineRule('min', min)
+        defineRule('max', max)
+        defineRule('alpha_spaces', alphaSpaces)
+        defineRule('email', email)
+        defineRule('max_value', maxValue)
+        defineRule('min_value', minValue)
+        defineRule('password_mismatch', confirmed)
+        defineRule('excluded', excluded)
+         defineRule('country_excluded', excluded)
+
+        configure({
+            generateMessage: (ctx) => {
+                const messages = {
+                  required: `The field ${ctx.field} is required`,
+                  min: `The field ${ctx.field} is to short`,
+                  max: `The field ${ctx.field} is to long`,
+                  alpha_spaces: `The field ${ctx.field} may only contain alphabetic character and spaces`,
+                  email: `The field ${ctx.field} must be a valide email`,
+                  max_value: `The field ${ctx.field} is too high.`,
+                  min_value: `The field ${ctx.field} is too low .`,
+                  confirmed: `The field ${ctx.field} `,
+                  excluded: `You are not allowed to use this value for the field ${ctx.field} `,
+                  country_excluded: `Due to restrictions we do not accept users from this location.`,
+                  password_mismatch: "The passwords do not match.",
+                  tos: "You must accept the terms of service.",
+                };
+
+                const message = messages[ctx.rule.name] ?  messages[ctx.rule.name] : `The field ${ctx.field} is invalid`;
+
+                    return message;
+            }
+        })
     }
 }
