a1f8963cdae4f6718a2e0c05bc5adbccf1d21037
Newton Nyamu <newtonnyamu@Newtons-MacBook-Pro.local>
2024-01-03 13:05:13 +0300

    install and configure firebase

---
+++ src/includes/firebase.js
@@ -0,0 +1,15 @@
+import firebase from "firebase/app";
+import 'firbase/auth'
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDjWqxqgfGqXPHMBkCsnMbL27U7LqeSNUs",
+  authDomain: "beat-muziki-app.firebaseapp.com",
+  projectId: "beat-muziki-app",
+  storageBucket: "beat-muziki-app.appspot.com",
+  messagingSenderId: "360199109713",
+  appId: "1:360199109713:web:d5224df02d8931812c8c70"
+};
+
+// Initialize Firebase
+export default firebase.initializeApp(firebaseConfig);
