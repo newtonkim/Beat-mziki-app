6c24c76336a4b4e29b5cb87c143c28cc8fe04b89
Newton Nyamu <newtonnyamu@Newtons-MacBook-Pro.local>
2024-01-04 07:55:56 +0300

    refactor firebase.js file

--- src/includes/firebase.js
+++ src/includes/firebase.js
@@ -1,15 +1,21 @@
-import firebase from "firebase/app";
+import firebase from 'firebase/app'
 import 'firebase/auth'
+import 'firebase/firestore' //newer database for firebase
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDjWqxqgfGqXPHMBkCsnMbL27U7LqeSNUs",
-  authDomain: "beat-muziki-app.firebaseapp.com",
-  projectId: "beat-muziki-app",
-  storageBucket: "beat-muziki-app.appspot.com",
-  messagingSenderId: "360199109713",
-  appId: "1:360199109713:web:d5224df02d8931812c8c70"
-};
+  apiKey: 'AIzaSyDjWqxqgfGqXPHMBkCsnMbL27U7LqeSNUs',
+  authDomain: 'beat-muziki-app.firebaseapp.com',
+  projectId: 'beat-muziki-app',
+  storageBucket: 'beat-muziki-app.appspot.com',
+  messagingSenderId: '360199109713',
+  appId: '1:360199109713:web:d5224df02d8931812c8c70'
+}
 
 // Initialize Firebase
-export default firebase.initializeApp(firebaseConfig);
+firebase.initializeApp(firebaseConfig)
+
+const auth = firebase.auth()
+const db = firebase.firestore()
+
+export { auth, db }
